c1d5bb0ce2070cb8242ca652b0a139e2d37ac724
Amanda Elen Santos Silva <amanda.elensantos@gmail.com>
2023-10-17 20:28:18 -0300

    aula 6 - componentização

--- src/App.jsx
+++ src/App.jsx
@@ -1,119 +1,74 @@
 import { useState } from 'react'
-import React from 'react';
-import './App.css';
-/*import dataCategories from "./data/data-categories.json"; 
-import dataMembers from "./data/data-members.json"; */
-import dataTasks from "./data/data-tasks.json"; 
+import './App.css'
+import dataTasks from '../src/data/data-tasks.json'
+/* import dataCategories from '../src/data/data-categories.json'
+import dataMembers from '../src/data/data-members.json' */
 import { Footer, TaskForm, TaskList } from './components'
-
+//import '../src/excercises/class2'
 
 function App() {
 
-  const [categories] = useState(dataCategories);
-  /**/const [members] = useState(dataMembers);
+
   const [tasks, setTasks] = useState(dataTasks);
-  const [currentTask, setCurrentTask] = useState("");
+/*   const [categories] = useState(dataCategories);
+  const [members] = useState(dataMembers); */
 
-const handleSubmit = (event) =>{
-  event.preventDefault(); //Impede o navegador de recarregar a pagina 
+  
+  const addTask = (title, category, member) => {
+    if(!title || !category || !member) {
+      alert ("Todos os campos são obrigatórios");
+      return;
+    }
 
+    const newTask = [
+      ...tasks,
+      {
+        id: Math.floor(Math.random() * 10000),
+        title,
+        category,
+        member,
+        status: "todo"
+      }
+    ]
 
-//validação dos campos
-if(!currentTask){
-  alert("Todos os campos são obrigatórios")
-  return;
-  } else{
-  alert(`Você digitou: ${currentTask}`)
-  return;
+    setTasks(newTask);
   }
-}
-//console.log(currentTask)
-  return(
-    //react sempre tem que ter uma tag mae
+
+  const deleteTask = (id) => {
+    const newTasks = [...tasks];
+    const filteredTasks = newTasks.filter(task => task.id !== id ? task : null)
+    setTasks(filteredTasks);
+  }
+
+  const startTask = (id) => {
+    const newTasks = [...tasks];
+    newTasks.map((task) => 
+      task.id === id ? (task.status = 'todo') : task
+    );
+    setTasks(newTasks);
+  }
+
+  const closeTask = (id) => {
+    const newTasks = [...tasks];
+    newTasks.map((task) => 
+      task.id === id ? (task.status = 'done') : task
+    );
+    setTasks(newTasks);
+  }
+
+  return (
     <div>
-    <section className='section-main'>
-      <div className='container-card'>
-        <h1> Cadastrar tarefa</h1>
-        <hr/>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor='title'>Titulo</label>
-          <input
-          type='text'
-          name='title'
-          id='title'
-          placeholder='Digite o titulo da tarefa' 
-          value={currentTask}
-          onChange={(event) => setCurrentTask(event.target.value)}
-          />
-          <label htmlFor='category'>Categoria</label>
-          <select name='category' id='category'>
-            <option value="">Selecione uma categoria</option>
-            {categories && categories.map((category) => {
-                return (
-                  <React.Fragment key={category.id}>
-                    <option value={category.title}>{category.title}</option>
-                  </React.Fragment>
-                )
-              })}
-            <option value="UX/UI">UX/UI</option>
-            <option value="Testes">Testes</option>
-          </select>
-          <label htmlFor='member'>Membros</label>
-          <select name='member' id='member'>
-            <option value="">Selecione um membro da equipe</option>
-              {members && members.map((member) => {
-                  return (
-                    <React.Fragment key={member.id}>
-                      <option value={member.profile}>{member.name}</option>
-                    </React.Fragment>
-                  )
-                })}
-          </select>
-          <button className='btn-register' type='submit'> Cadastrar</button>
-        </form>
-      </div>
-    </section>
-    <section className='section-main'>
-      <div className='container-card'>
-        <h1>Lista de tarefas</h1>
-        <hr/>
-        {tasks && tasks.map((task) => {
-            return (
-              <React.Fragment key={task.id}>
-                <div className='task'>
-                  <img src={`https://github.com/${task.member}.png`} className='task-img-member'/>
-                  <div className='task-content'>
-                    <h2 className='task-title'>{task.title}</h2>
-                    <span className='task-member'>{task.member}</span>
-                    <span className='task-category'>{task.category}</span>
-                    <span className={`task-${task.status}`}>{task.status}</span>
-                  </div>
-                  <div className='task-actions'>
-                    <button className='btn-start'>Iniciar</button>
-                    <button className='btn-close'>Finalizar</button>
-                    <button className='btn-delete'>x</button>
-                  </div>
-                </div>
-                <hr />
-              </React.Fragment>
-            )
-          })}
-      </div>
-    </section>
-    <footer className='footer-main'>
-      <p>
-        <strong>
-          Introducao ao Desenvolvimento 
-          Front-End com React
-        </strong>
-      </p>
-      <p>
-        <em>
-          Professor: Anderson Carvalho
-        </em>
-      </p>
-    </footer>
+      <TaskForm addTask={addTask} />
+      <TaskList
+        tasks={tasks}
+        deleteTask={deleteTask}
+        startTask={startTask}
+        closeTask={closeTask}
+      />
+      <Footer />
+      
     </div>
   )
 }
+
 export default App
